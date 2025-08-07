91a56dd66aa669fe87018aa3adbe8295d9d9b39f
Bernard Krehula <krehula.bernard456@gmail.com>
2025-08-07 19:28:54 +0200

    Display reshaped inputs

--- my-app/src/SingleForm.jsx
+++ my-app/src/SingleForm.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 
 const SingleForm = ({name, formInfo, initialized, updateFormInfo}) => {
 
@@ -6,20 +7,33 @@ const SingleForm = ({name, formInfo, initialized, updateFormInfo}) => {
         updateFormInfo(name, fieldName, value);  
     }
     
-    if(initialized.current) console.log(Object.values(formInfo[name]))
+   
 
     return(
         <form>
-            {initialized.current ? Object.values(formInfo[name]).map((info, index) => {
-                const { id, name, placeHolder, value} = info;
-                console.log(info)
-                return(
-                    <input key={id} name={name} placeholder={placeHolder} onChange={handleOnChange}></input>
+            {initialized.current ? (
+                name === 'personal' ? (
+                    Object.values(formInfo[name]).map((info, index) => {
+                    const { name: nameInfo, placeHolder, value } = info;
+                    return (
+                        <input key={index} name={nameInfo} placeholder={placeHolder} value={value} onChange={handleOnChange}/>
+                    );
+                    })
+                ) : (
+                    formInfo[name].map((infoGroup, groupIndex) => {
+                    return (
+                        <React.Fragment key={groupIndex}>
+                        {Object.entries(infoGroup).map(([key, info]) => {
+                            const { id, name: nameInfo, placeHolder, value } = info;
+                            return (
+                            <input key={id || key} name={nameInfo} placeholder={placeHolder} value={value} onChange={handleOnChange}/>
+                            );
+                        })}
+                       </React.Fragment>
+                    );
+                    })
                 )
-            })
-            :
-            ''
-            }
+                ) : null}
         </form>
     )
 }
