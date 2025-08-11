eca7336b0aabb934c425e05ca0ab51b353421f30
Bernard Krehula <krehula.bernard456@gmail.com>
2025-08-11 15:55:31 +0200

    Npm print installed

--- my-app/src/SinglePreview.jsx
+++ my-app/src/SinglePreview.jsx
@@ -1,11 +1,23 @@
 import SingleBtn from "./SingleBtn";
+import './SinglePreview.css'
 
 const SinglePreview = ({formInfo}) => {
     return(
         <>
             <div className="main-preview">
+                <SingleBtn>Print</SingleBtn>
                 <SingleBtn></SingleBtn>
-                <SingleBtn></SingleBtn>
+                <div className="main-info">
+                    <div>
+
+                    </div>
+                    <div lclassName='presonal-details'>
+                        <h1>Personal Details</h1>
+                        <h2>Adress</h2>
+                        <h2>Phone Number</h2>
+                        <h2>Email</h2>
+                    </div>
+                </div>
             </div>
         </>
     )
