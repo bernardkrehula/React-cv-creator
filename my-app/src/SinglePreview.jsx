56c6d64d5468169d190f029d4f14b36c2d337d73
Bernard Krehula <krehula.bernard456@gmail.com>
2025-08-12 19:33:11 +0200

    Print cv form

--- my-app/src/SinglePreview.jsx
+++ my-app/src/SinglePreview.jsx
@@ -1,11 +1,13 @@
+import { use } from "react";
 import SingleBtn from "./SingleBtn";
 import './SinglePreview.css'
 import { useRef, useState } from "react";
-import { useReactToPrint } from "react-to-print";
+import { useReactToPrint } from 'react-to-print';
 
 const SinglePreview = ({formInfo, handleBackPreview}) => {
     const formKeys = useRef([]);
-    const infoNames = formNames.current;
+    const infoNames = formKeys.current;
+    const contentRef = useRef();
     const personalDetailsNames = ['adress', 'phoneNumber', 'email'];
     
     const iterateFormNames = Object.keys(formInfo).reduce((acc, key) => {
@@ -14,14 +16,17 @@ const SinglePreview = ({formInfo, handleBackPreview}) => {
         return acc;
     }, [])
     
+    
+    const reactToPrintFn = useReactToPrint({ contentRef });
+    
     return(
-        <>
+        <>  
             <div className="main-preview">
                 <div className="printBtns">
-                    <SingleBtn variation='printBtn'><svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="icon icon-tabler icons-tabler-outline icon-tabler-printer"><path stroke="none" d="M0 0h24v24H0z" /><path d="M17 17h2a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h2M17 9V5a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v4" /><path d="M7 15a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2H9a2 2 0 0 1-2-2z" /></svg>Print</SingleBtn>
+                    <SingleBtn variation='printBtn' onClick={reactToPrintFn}><svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="icon icon-tabler icons-tabler-outline icon-tabler-printer"><path stroke="none" d="M0 0h24v24H0z" /><path d="M17 17h2a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h2M17 9V5a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v4" /><path d="M7 15a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2H9a2 2 0 0 1-2-2z" /></svg>Print</SingleBtn>
                     <SingleBtn variation='exitBtn' onClick={handleBackPreview}><svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="icon icon-tabler icons-tabler-outline icon-tabler-arrow-back-up"><path stroke="none" d="M0 0h24v24H0z" /><path d="m9 14-4-4 4-4" /><path d="M5 10h11a4 4 0 1 1 0 8h-1" /></svg>Back</SingleBtn>
                 </div>
-                <div className="main-info">
+                <div className="main-info" ref={contentRef}>
                     <div className="green-space"/>
                     <div className="info-space">
                         <ul className="job-info">
